21ac1a3d73f4998ce92af8f47ef2542c1a215e3e
sjaraya <sergaraya22@gmail.com>
2017-09-21 10:23:56 -0600

    Maps fixing

--- app/src/main/java/com/example/jeison/triangulacion_aps/Dashboard.java
+++ app/src/main/java/com/example/jeison/triangulacion_aps/Dashboard.java
@@ -5,19 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
 
 public class Dashboard extends AppCompatActivity {
 
@@ -94,17 +93,16 @@ public class Dashboard extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
 
+            View rootView;
+
             if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
-                View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
-                TextView textViewOne = (TextView) rootView.findViewById(R.id.camera);
+
+                rootView = inflater.inflate(R.layout.fragment_camera, container, false);
                 return rootView;
+
             } else {
-                // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-                //fragmentManager.beginTransaction().replace(R.id.f, new GmapsFragment()).commit();
 
-
-                GmapsFragment gmapsFragment = new GmapsFragment();
-                View rootView = inflater.inflate(R.layout.fragment_gmaps, container, false);
+                rootView = inflater.inflate(R.layout.fragment_gmaps, container, false);
                 return rootView;
             }
         }
@@ -124,7 +122,16 @@ public class Dashboard extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            //return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0:
+                    return PlaceholderFragment.newInstance(position + 1);
+                case 1:
+                    GmapsFragment gmapsFragment = new GmapsFragment();
+                    return gmapsFragment;
+                default:
+                    return null;
+            }
         }
 
         @Override
--- app/src/main/java/com/example/jeison/triangulacion_aps/GmapsFragment.java
+++ app/src/main/java/com/example/jeison/triangulacion_aps/GmapsFragment.java
@@ -1,23 +1,20 @@
 package com.example.jeison.triangulacion_aps;
 
 import android.app.Fragment;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -25,24 +22,27 @@ import com.google.android.gms.maps.model.MarkerOptions;
  * Created by SergioAraya on 31/08/2017.
  */
 
-public class GmapsFragment extends Fragment implements OnMapReadyCallback {
+public class GmapsFragment extends android.support.v4.app.Fragment implements OnMapReadyCallback {
 
-    private GoogleMap gMap;
+    SupportMapFragment mapFragment;
+    GoogleMap gMap;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_gmaps, container, false);
+
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
         super.onViewCreated(view, savedInstanceState);
 
-        MapFragment mapFragment = (MapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
 
     @Override
@@ -52,17 +52,12 @@ public class GmapsFragment extends Fragment implements OnMapReadyCallback {
 
         LatLng santaClara = new LatLng(10.3652, -84.512);
 
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(santaClara, 13));
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(santaClara, 18));
         gMap.addMarker(new MarkerOptions().title("Welcome to Santa Clara!").position(santaClara));
 
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    /*@Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_gmaps, menu);
         super.onCreateOptionsMenu(menu, inflater);
@@ -85,6 +80,5 @@ public class GmapsFragment extends Fragment implements OnMapReadyCallback {
                 return true;
         }
         return false;
-    }*/
-
+    }
 }
